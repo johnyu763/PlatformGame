035df50f83539b9052940810afc07904b7f70204
something999 <wongny1@uci.edu>
2019-04-27 13:43:44 -0700

    Added scene for pause menu

---
+++ VGDCPlatformer/Assets/PauseMenu.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseMenu : MonoBehaviour
+{
+    public static bool isPaused = false;
+    public GameObject PauseScreen;
+
+    public void Update()
+    { 
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (!isPaused)
+            {
+                Pause();
+            }
+            else
+            {
+                Resume();
+            }
+        }
+    }
+
+    public void Pause()
+    {
+        PauseScreen.SetActive(true);
+        Time.timeScale = 0f;
+        isPaused = true;
+    }
+
+    public void Resume()
+    {
+        PauseScreen.SetActive(false);
+        Time.timeScale = 1f;
+        isPaused = false;
+    }
+
+    public void Quit()
+    {
+        Application.Quit();
+    }
+
+}
