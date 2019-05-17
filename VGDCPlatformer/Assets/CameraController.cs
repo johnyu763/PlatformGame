e0b8d460d823835d799943d97806e1fdac6972e4
something999 <wongny1@uci.edu>
2019-05-16 20:00:51 -0700

    Slightly smoother camera

--- VGDCPlatformer/Assets/CameraController.cs
+++ VGDCPlatformer/Assets/CameraController.cs
@@ -5,21 +5,23 @@ public class CameraController : MonoBehaviour
 {
 
     public GameObject player;
+    public GameObject board;
     private Vector3 offset;
     private Rigidbody rb;
 
     void Start()
     {
-       offset = transform.position - player.transform.position;
-       rb = player.gameObject.GetComponent<Rigidbody>();
+        offset = transform.position - player.transform.position;
+        rb = player.gameObject.GetComponent<Rigidbody>();
     }
 
     void FixedUpdate()
     {
-       
-        Vector3 velocity = rb.velocity;
+
+        Vector3 velocity = rb.velocity.normalized;
         float angle = Mathf.Atan2(velocity.x, velocity.z);
         transform.rotation = Quaternion.Euler(transform.localEulerAngles.x, Mathf.Rad2Deg * angle, transform.localEulerAngles.z);
-       transform.position = player.transform.position + offset;
+        transform.position = player.transform.position + offset;
     }
+
 }
